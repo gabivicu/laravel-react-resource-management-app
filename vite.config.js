5e34fa2c09b0ee318863cc2136e1a3c3f3896af1
StayStrong <staystrong@MacBook-Pro-StayStrong.local>
2026-01-22 12:13:08 +0200

    Fix Docker and Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,11 @@ export default defineConfig({
         },
     },
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost',
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
