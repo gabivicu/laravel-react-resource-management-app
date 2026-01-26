ced485a16280edfdacadf530a3767c1384d106cf
StayStrong <staystrong@MacBook-Pro-StayStrong.local>
2026-01-26 16:36:04 +0200

    Fix PermissionFactory to generate unique slugs using UUID

--- database/factories/PermissionFactory.php
+++ database/factories/PermissionFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Domains\Permission\Models\Permission;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class PermissionFactory extends Factory
 {
@@ -14,7 +15,9 @@ class PermissionFactory extends Factory
         $groups = ['projects', 'tasks', 'users', 'resources', 'organizations'];
         $group = $this->faker->randomElement($groups);
         $action = $this->faker->randomElement(['create', 'read', 'update', 'delete', 'manage']);
-        $slug = "{$group}.{$action}";
+        // Generate unique slug using UUID to avoid conflicts with seeder permissions
+        $uniqueId = Str::uuid()->toString();
+        $slug = "{$group}.{$action}.{$uniqueId}";
 
         return [
             'name' => ucfirst($action).' '.ucfirst($group),
